09b65f1b31d0afd7f7386513560f4ff1012adf27
Chi Min Yong <chimin.yong@g2g.com>
2021-01-14 17:26:07 +0800

    Add main function

---
+++ snake/src/main/Functions/main.cpp
@@ -0,0 +1,34 @@
+// Copyright ii887522
+
+#ifndef TEST
+
+#include <Any/allocator.h>
+#include <SDL.h>
+#include <Any/Subsystems.h>
+#include <Any/App.h>
+#include <Struct/Size.h>
+#include <Functions/control_flow.h>
+#include "../Factory/SnakeViewGroupFactory.h"
+
+using ii887522::viewify::Subsystems;
+using ii887522::viewify::App;
+using ii887522::viewify::Size;
+using ii887522::viewify::eventLoop;
+
+namespace ii887522::snake {
+
+static int main(int, char**) {
+  const Subsystems subsystems;
+  constexpr auto viewCount{ 2u };
+  SnakeViewGroupFactory<viewCount> snakeViewGroupFactory;
+  eventLoop(App<viewCount>{ "Snake", Size{ 752, 752 }, &snakeViewGroupFactory });
+  return EXIT_SUCCESS;
+}
+
+}  // namespace ii887522::snake
+
+int main(int argc, char** argv) {
+  return ii887522::snake::main(argc, argv);
+}
+
+#endif
