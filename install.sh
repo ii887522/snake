74c7a5e762088644e99b7d4194e1ca59ff9bdd76
Chi Min Yong <chimin.yong@g2g.com>
2021-01-17 11:10:09 +0800

    It only support Windows

--- install.sh
+++
@@ -1,3 +0,0 @@
-python -m pip install virtualenv
-virtualenv env
-source env/bin/activate && python -m pip install cpplint && source env/bin/deactivate && docker run --rm --name snake_installer -w C:/snake -v $PWD:C:/snake snake-custom-node ncu.cmd -u && npm.cmd install
